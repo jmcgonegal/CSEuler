8f3e0df52d2f78c610706c0ac9363a33a8d4756f
John McGonegal <mcgonegal@gmail.com>
2011-03-03 16:27:20 -0700

    problem 40 solved

---
+++ Problem40/Problem40.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem40
+{
+    class Problem40
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            int digits = 0;
+            int product = 1;
+            while (digits <= 1000000)
+            {
+                string number = "" + count;
+                foreach (char c in number)
+                {
+                    digits++;
+                    int num = int.Parse(""+c);
+                    switch (digits)
+                    {
+                        case 1:
+                        case 10:
+                        case 100:
+                        case 1000:
+                        case 10000:
+                        case 100000:
+                        case 1000000:
+                            product *= num;
+                            break;
+                    }
+                }
+                count++;
+            }
+            Console.WriteLine("Answer = " + product);
+            Console.ReadKey();
+        }
+    }
+}
