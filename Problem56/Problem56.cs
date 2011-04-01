98bd24d97e83799898eeb401ccbc117c1ff1fdd0
John McGonegal <mcgonegal@gmail.com>
2011-04-01 11:01:36 -0700

    56 solved

---
+++ Problem56/Problem56.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Problem56
+{
+    class Problem56
+    {
+        static void Main(string[] args)
+        {
+            int max_sum = 0;
+            for (int a = 1; a < 100; a++)
+            {
+                for (int b = 1; b < 100; b++)
+                {
+                    BigInteger num = BigInteger.Pow(a,b);
+                    int sum = 0;
+                    foreach (char digit in num.ToString())
+                    {
+                        sum += int.Parse("" + digit);
+                    }
+                    if (max_sum < sum)
+                    {
+                        max_sum = sum;
+                    }
+                    
+                }
+            }
+            Console.WriteLine("Answer = " + max_sum);
+            Console.ReadKey();
+        }
+    }
+}
