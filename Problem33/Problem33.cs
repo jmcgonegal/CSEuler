3047140dac5e6cb5c329ee997df2859e0cc90e85
John McGonegal <mcgonegal@gmail.com>
2011-04-01 10:21:38 -0700

    33 and 92 solved, welcome to level 2!

---
+++ Problem33/Problem33.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem33
+{
+    class Problem33
+    {
+        static void Main(string[] args)
+        {
+            int answer_n = 1, answer_d = 1;
+            for (int d = 11; d <= 99; d++)
+            {
+                for (int n = 10; n < d; n++)
+                {
+                    if (n % 10 != 0) // 10/20  is too simple
+                    {
+                        double answer = 1.0 * n / d;
+                        string nu = "" + n;
+                        string de = "" + d;
+                        int top = 0, bottom = 0;
+                        if (nu[0] == de[0])
+                        {
+                            top = int.Parse("" + nu[1]);
+                            bottom = int.Parse("" + de[1]);
+                        }
+                        else if (nu[1] == de[1])
+                        {
+                            top = int.Parse("" + nu[0]);
+                            bottom = int.Parse("" + de[0]);
+                        }
+                        else if (nu[1] == de[0])
+                        {
+                            top = int.Parse("" + nu[0]);
+                            bottom = int.Parse("" + de[1]);
+                        }
+                        else if (nu[0] == de[1])
+                        {
+                            top = int.Parse("" + nu[1]);
+                            bottom = int.Parse("" + de[0]);
+                        }
+                        if (bottom != 0 && top != 0)
+                        {
+                            double test = 1.0 * top / bottom;
+                            if (test == answer)
+                            {
+                                answer_n *= top;
+                                answer_d *= bottom;
+                                Console.WriteLine(n + "/" + d + " = " + top + "/" + bottom);
+                            }
+                        }
+                    }
+                }
+                
+            }
+            // 8/800 = 1/100, lazy
+            Console.WriteLine(answer_n + "/" + answer_d + " Answer = " + answer_d / answer_n);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Problem92/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Problem92
+{
+    class Program
+    {
+        static int getSquareSum(int value)
+        {
+            int result = 0;
+            string digits = value.ToString();
+            foreach (char c in digits)
+            {
+                int val = int.Parse("" + c);
+                result += val*val;
+            }
+            
+            return result;
+
+        }
+        static void Main(string[] args)
+        {
+            const int MAX = 10000000;
+            int count_89 = 0;
+
+            // BitArrays are fast
+            BitArray arrive_at_89 = new BitArray(MAX);
+            BitArray arrive_at_1 = new BitArray(MAX);
+            arrive_at_1[1] = true;
+            arrive_at_89[89] = true;
+            for (int i = 1; i < MAX; i++)
+            {
+                bool repeat = false;
+
+                List<int> squares = new List<int>();
+                int test = i;
+               
+                while (!repeat)
+                {
+                    squares.Add(test);
+                    if (arrive_at_1[test])
+                    {
+                        foreach (int sq in squares)
+                        {
+                            arrive_at_1[sq] = true;
+                        }
+                        repeat = true;
+                    } 
+                    else if (arrive_at_89[test])
+                    {
+                        count_89++;
+                        foreach (int sq in squares)
+                        {
+                            arrive_at_89[sq] = true;
+                        }
+                        repeat = true;
+                    }
+
+                    test = getSquareSum(test);
+                }
+
+
+            }
+            Console.WriteLine("Answer = " + count_89);
+            Console.ReadKey();
+        }
+    }
+}
